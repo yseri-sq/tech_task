f8cb0e3a6ee944a9270b3543fad76bb056d94411
yseri-sq <yseri.sq@gmail.com>
2026-09-23 09:59:29 +0400

    edit-page add ref

--- application/services/request_service.py
+++ application/services/request_service.py
@@ -1,10 +1,15 @@
 from application.interfaces.request_repository import RequestRepository
 
+
 class RequestService:
+    
     def __init__(self, repository: RequestRepository):
         self.repository = repository
 
     def get_request(self, request_id):
+        from presentation.pages.edit_page import enter_num_col
+        enter_num_col.current.visible = False
+        enter_num_col.current.update()
         return self.repository.get_by_id(request_id)
 
     def save_request(
--- presentation/pages/edit_page.py
+++ presentation/pages/edit_page.py
@@ -2,11 +2,24 @@ import flet as ft
 from core.dependencies import get_request_service
 # page = ft.Page
 
+enter_num_col = ft.Ref[ft.Column]()
+result_col = ft.Ref[ft.Column]()
+
+
+def on_ecler_click():
+    enter_num_col.current.visible = True
+    result_col.current.visible = False
+
+    enter_num_col.current.update()
+    result_col.current.update() 
 
 def edit_page():
     order_num = ft.TextField(label="Enter №")
-    result = ft.Column()
-
+    result = ft.Column(
+        ref=result_col,
+        controls=[]
+    )
+ 
     def check_order(e):
         if not order_num.value:
             print("ERR")
@@ -21,30 +34,66 @@ def edit_page():
         if request is None:
             print("Not found")
         else:
-            result.controls.append(
-                ft.TextField(label="Client", value=request.client)
-            )
-            result.controls.append(
-                ft.TextField(label="Equipment", value=request.equipment)
-            )
-            result.controls.append(
-                ft.TextField(label="Faul Type", value=request.fault_type)
-            )
-            result.controls.append(
-                ft.TextField(label="Description", value=request.description)
-            )
-            result.controls.append(
-                ft.TextField(label="Status", value=request.status)
+            result_col.current.controls.append(
+                ft.Container(
+                    ft.Column(
+                        controls=[
+                            ft.Row(
+                                controls=[
+                                    ft.Row(
+                                        controls=[
+                                            ft.TextField(label="Client", value=request.client),
+                                            ft.TextField(label="Equipment", value=request.equipment)
+                                        ]
+                                    )                       
+                                ]
+                            ),
+                            ft.Row(
+                                controls=[
+                                    ft.Row(
+                                        controls=[
+                                            ft.TextField(label="Faul Type", value=request.fault_type),
+                                            ft.TextField(label="Description", value=request.description)
+                                        ]
+                                    )                       
+                                ]
+                            ),
+                            ft.Row(
+                                controls=[
+                                    ft.Row(
+                                        controls=[
+                                            ft.TextField(label="Status", value=request.status),
+                                            ft.Button("eclear", on_click=on_ecler_click),
+                                            ft.Button("edit", on_click=...)
+                                        ]
+                                    )                      
+                                ]
+                            )
+                        ]
+                    )
+                )
             )
         e.page.update()
 
     return ft.Container(
-        content=
-            ft.Column(
-                controls=[
-                    order_num,
-                    ft.Button("Check", on_click=check_order),
-                    result
-                ]
-            )   
+        content=ft.Row(
+            controls=[
+                ft.Column(
+                    ref=enter_num_col,
+                    controls=[
+                        order_num,
+                        ft.Button("Check", on_click=check_order),
+                    ]
+                ),
+                ft.Column(
+                    controls=[
+                        ft.Row(
+                            controls=[
+                                result
+                            ]
+                        )
+                    ]
+                )
+            ]
         )
+    )
